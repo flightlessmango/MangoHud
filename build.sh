d5e06975b6d8368d5857e917fb7bbd24b3c22b2b
Guilherme Silva <626206+guihkx@users.noreply.github.com>
2020-02-08 09:52:51 -0300

    Add Zorin OS to build script

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ dependencies() {
                 DEPS="{glibc-devel.i686,libstdc++-devel.i686,libX11-devel.i686}"
                 install
             ;;
-            *"buntu"|"Linux Mint"|"Debian")
+            *"buntu"|"Linux Mint"|"Debian"|"Zorin OS")
                 MANAGER_QUERY="dpkg-query -s"
                 MANAGER_INSTALL="apt install"
                 DEPS="{gcc,g++,gcc-multilib,g++-multilib,ninja-build,python3-pip,python3-setuptools,python3-wheel,pkg-config,mesa-common-dev,libx11-dev:i386}"
