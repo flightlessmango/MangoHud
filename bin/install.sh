fda9f1cd94b105eccb9003a4d74334baf303267e
telans <telans@protonmail.com>
2020-02-13 10:27:53 +1300

    install: recursive

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 
 cp -rv ".local" "$HOME/"
 if [[ ! -f "$HOME/.config/MangoHud/MangoHud.conf" ]]; then
-    cp -v ".config" "$HOME/"
+    cp -rv ".config" "$HOME/"
 fi
 
 sed -i "s|libMangoHud.so|$HOME/.local/share/MangoHud/libMangoHud32.so|g" "$HOME/.local/share/vulkan/implicit_layer.d/mangohud32.json"
