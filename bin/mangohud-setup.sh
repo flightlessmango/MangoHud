69947c37328ab4b7502da4b9d057a4047abb13a5
jackun <jack.un@gmail.com>
2020-03-11 19:01:59 +0200

    Update mangohud-setup.sh for new libdir paths

--- bin/mangohud-setup.sh
+++ bin/mangohud-setup.sh
@@ -9,9 +9,10 @@ install() {
 
 uninstall() {
     [ "$UID" -eq 0 ] || exec sudo bash "$0" uninstall
-    rm -rfv "/usr/lib/MangoHud"
+    rm -rfv "/usr/lib/mangohud"
     rm -fv "/usr/share/vulkan/implicit_layer.d/mangohud.json"
     rm -fv "/etc/ld.so.conf.d/libmangohud.conf"
+    rm -fv "/etc/ld.so.conf.d/lib32-libmangohud.conf"
     rm -fv "/usr/bin/mangohud"
 }
 
