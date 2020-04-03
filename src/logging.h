4830268a1118d9da2327ee329180fdc2721e62be
André Almeida <andrealmeid@collabora.com>
2020-04-03 17:28:45 -0300

    logging: fix indentation

--- src/logging.h
+++ src/logging.h
@@ -41,16 +41,21 @@ void *logging(void *params_void){
   overlay_params *params = reinterpret_cast<overlay_params *>(params_void);
   time_t now_log = time(0);
   tm *log_time = localtime(&now_log);
-	string date = to_string(log_time->tm_year + 1900) + "-" + to_string(1 + log_time->tm_mon) + "-" + to_string(log_time->tm_mday) + "_" + to_string(1 + log_time->tm_hour) + "-" + to_string(1 + log_time->tm_min) + "-" + to_string(1 + log_time->tm_sec);
+  string date = to_string(log_time->tm_year + 1900) + "-" +
+                to_string(1 + log_time->tm_mon) + "-" +
+                to_string(log_time->tm_mday) + "_" +
+                to_string(1 + log_time->tm_hour) + "-" +
+                to_string(1 + log_time->tm_min) + "-" +
+                to_string(1 + log_time->tm_sec);
   log_start = os_time_get();
   out.open(params->output_file + date, ios::out | ios::app);
   out << "os," << "cpu," << "gpu," << "ram," << "kernel," << "driver" << endl;
   out << os << "," << cpu << "," << gpu << "," << ram << "," << kernel << "," << driver << endl;
-	while (loggingOn){
+  while (loggingOn){
     uint64_t now = os_time_get();
     elapsedLog = (double)(now - log_start);
     out << fps << "," << cpuLoadLog << "," << gpuLoadLog << "," <<  now - log_start << endl;
-		// logArray.push_back({fps, cpuLoadLog, gpuLoadLog, 0.0f});
+    // logArray.push_back({fps, cpuLoadLog, gpuLoadLog, 0.0f});
 
     if ((elapsedLog) >= params->log_duration * 1000000 && params->log_duration)
       loggingOn = false;
