fea9980d065dc77313e57988f4cc87a1d5eb945d
FlightlessMango <flightlessmangoyt@gmail.com>
2020-04-05 13:47:27 +0200

    [Build script] *Buntu no longer needs libx11-dev 32bit

--- build.sh
+++ build.sh
@@ -67,7 +67,7 @@ dependencies() {
             *"buntu"|"Linux Mint"|"Debian"|"Zorin OS"|"Pop!_OS"|"elementary OS")
                 MANAGER_QUERY="dpkg-query -s"
                 MANAGER_INSTALL="apt install"
-                DEPS="{gcc,g++,gcc-multilib,g++-multilib,ninja-build,python3-pip,python3-setuptools,python3-wheel,pkg-config,mesa-common-dev,libx11-dev:i386,libxnvctrl-dev}"
+                DEPS="{gcc,g++,gcc-multilib,g++-multilib,ninja-build,python3-pip,python3-setuptools,python3-wheel,pkg-config,mesa-common-dev,libx11-dev,libxnvctrl-dev}"
                 install
                 
                 if [[ $(sudo pip3 show meson; echo $?) == 1 || $(sudo pip3 show mako; echo $?) == 1 ]]; then
