f9b5e233eddd7dc667ec80eac3bca355c4b9a172
medusalix <ss7@live.de>
2020-04-09 14:03:11 +0200

    Improve os-release parsing

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ for os_release in ${OS_RELEASE_FILES[@]} ; do
     if [[ ! -e "${os_release}" ]]; then
         continue
     fi
-    DISTRO=$(sed 1q ${os_release} | sed 's/NAME=//g' | sed 's/"//g')
+    DISTRO=$(sed -rn 's/^NAME="(.+)"/\1/p' ${os_release})
 done
 
 dependencies() {
