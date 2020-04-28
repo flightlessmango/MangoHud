41e15fba5a18b97bfd3a7a2061f2cf50478c6238
FlightlessMango <flightlessmangoyt@gmail.com>
2020-04-28 17:02:51 +0200

    [win32] Inject dx11

--- src/kiero/main.cpp
+++ src/kiero/main.cpp
@@ -1,4 +1,6 @@
 #include "kiero.h"
+#include "windows.h"
+#include <iostream>
 
 #if KIERO_INCLUDE_D3D9
 # include "impl/d3d9_impl.h"
@@ -27,8 +29,19 @@
 
 #include <windows.h>
 
+void ConsoleSetup()
+{
+	// With this trick we'll be able to print content to the console, and if we have luck we could get information printed by the game.
+	AllocConsole();
+	SetConsoleTitle("MangoHud");
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+	freopen("CONIN$", "r", stdin);
+}
+
 int kieroExampleThread()
 {
+    ConsoleSetup();
     if (kiero::init(kiero::RenderType::Auto) == kiero::Status::Success)
     {
         switch (kiero::getRenderType())
@@ -61,12 +74,12 @@ int kieroExampleThread()
 
         return 1;
     }
-
 	return 0;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID)
 {
+    
     DisableThreadLibraryCalls(hInstance);
 
     switch (fdwReason)
