acf2d88fbcb7a7a47f957a87d20739c67049bd0d
Stephan Lachnit <stephanlachnit@protonmail.com>
2020-05-04 12:16:09 +0200

    Import Dear ImGui files as subproject
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- build-source.sh
+++ build-source.sh
@@ -3,12 +3,8 @@
 VERSION=$(git describe --tags --dirty)
 NAME=MangoHud-$VERSION-Source
 
-# ensure that submodules are present
-git submodule update --init
-# get everything except submodules
+# create archive via git
 git archive HEAD --format=tar --prefix=${NAME}/ --output=${NAME}.tar
-# add imgui submodule
-tar -rf ${NAME}.tar --exclude-vcs --transform="s,^modules/ImGui/src,${NAME}/modules/ImGui/src," modules/ImGui/src
 # create DFSG compliant version which excludes NVML
 cp ${NAME}.tar ${NAME}-DFSG.tar
 tar -f ${NAME}-DFSG.tar --delete ${NAME}/include/nvml.h
