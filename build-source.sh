80ce6eca628d25ce1f788f0ae1d8eebbb53ce384
Rémi Verschelde <rverschelde@gmail.com>
2020-06-11 13:22:44 +0200

    build-source.sh: Add parent directory to source tarballs
    Also ensure that submodules have been initialized.
    
    Generate tarballs in `build/` subfolder to avoid this error:
    ```
    tar: .: file changed as we read it
    ```

--- build-source.sh
+++ build-source.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 VERSION=$(git describe --tags --dirty=+)
+NAME=MangoHud-$VERSION-Source
 
 FILE_PATTERN="--exclude-vcs --exclude-vcs-ignores ."
 
+# Ensure that submodules are present
+git submodule update --init
+
+mkdir -p build
+
 # default version
-tar -czf MangoHud-$VERSION-Source.tar.gz $FILE_PATTERN
+tar -czf build/$NAME.tar.gz $FILE_PATTERN --transform "s,^\.,$NAME,"
 # DFSG compliant version, excludes NVML
-tar -czf MangoHud-$VERSION-Source-DFSG.tar.gz --exclude=include/nvml.h $FILE_PATTERN
+tar -czf build/$NAME-DFSG.tar.gz --exclude=include/nvml.h $FILE_PATTERN --transform "s,^\.,$NAME-DFSG,"
