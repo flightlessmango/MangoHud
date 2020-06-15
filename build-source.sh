8d0ee1264fc651d9cbb3152f291ae674a6ba3547
Stephan Lachnit <stephanlachnit@protonmail.com>
2020-06-15 14:52:49 +0200

    build-source.sh: only pack required imgui files
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- build-source.sh
+++ build-source.sh
@@ -7,8 +7,8 @@ NAME=MangoHud-$VERSION-Source
 git submodule update --init
 # get everything except submodules
 git archive HEAD --format=tar --prefix=${NAME}/ --output=${NAME}.tar
-# add imgui submodule
-tar -rf ${NAME}.tar --exclude-vcs --transform="s,^modules/ImGui/src,${NAME}/modules/ImGui/src," modules/ImGui/src
+# add imgui files
+tar -rf ${NAME}.tar --transform="s,^modules/ImGui/src,${NAME}/modules/ImGui/src," modules/ImGui/src/imconfig.h modules/ImGui/src/imgui.cpp modules/ImGui/src/imgui.h modules/ImGui/src/imgui_draw.cpp modules/ImGui/src/imgui_internal.h modules/ImGui/src/imgui_widgets.cpp modules/ImGui/src/imstb_rectpack.h modules/ImGui/src/imstb_textedit.h modules/ImGui/src/imstb_truetype.h
 # create DFSG compliant version which excludes NVML
 cp ${NAME}.tar ${NAME}-DFSG.tar
 tar -f ${NAME}-DFSG.tar --delete ${NAME}/include/nvml.h
