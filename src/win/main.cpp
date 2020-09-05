3ffa1bed7ae1381c0917e06d7b58fce3a803ae5b
FlightlessMango <flightlessmangoyt@gmail.com>
2020-09-05 07:51:36 +0200

    Windows attach dll

---
+++ src/win/main.cpp
@@ -0,0 +1,32 @@
+#include "windows.h"
+#include <cstdio>
+
+void ConsoleSetup()
+{
+	// With this trick we'll be able to print content to the console, and if we have luck we could get information printed by the game.
+	AllocConsole();
+	SetConsoleTitle("MangoHud");
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+	freopen("CONIN$", "r", stdin);
+}
+
+int MainThread(){
+    ConsoleSetup();
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID)
+{
+    
+    DisableThreadLibraryCalls(hInstance);
+
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MainThread, NULL, 0, NULL);
+        break;
+    }
+
+    return TRUE;
+}
