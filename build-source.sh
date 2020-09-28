ade319fe8769e8e43c11354a17e91a658bac6be5
Stephan Lachnit <stephanlachnit@protonmail.com>
2020-09-28 22:30:00 +0200

    ensure minihook is not in source tarballs
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- build-source.sh
+++ build-source.sh
@@ -5,6 +5,8 @@ NAME=MangoHud-$VERSION-Source
 
 # create archive via git
 git archive HEAD --format=tar --prefix=${NAME}/ --output=${NAME}.tar
+# remove unused minihook from source tarball
+tar -f ${NAME}.tar --delete ${NAME}/modules
 # create DFSG compliant version which excludes NVML
 cp ${NAME}.tar ${NAME}-DFSG.tar
 tar -f ${NAME}-DFSG.tar --delete ${NAME}/include/nvml.h
