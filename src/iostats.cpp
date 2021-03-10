8f3ecb81dfcd901b5793c0b15687146ee8b9af1e
Lukáš Horáček <flgx@protonmail.com>
2021-03-10 21:23:26 +0100

    Divide IO stats by time difference since last update
    This should prevent IO stats reaching high numbers when application stops responding resulting into MangoHud not updating.

--- src/iostats.cpp
+++ src/iostats.cpp
@@ -1,10 +1,17 @@
 #include "iostats.h"
 #include "string_utils.h"
+#include "timing.hpp"
 #include <fstream>
+
+Clock::time_point lastUpdate = Clock::now(); /* ns */
 
 void getIoStats(void *args) {
     iostats *io = reinterpret_cast<iostats *>(args);
     if (io) {
+        Clock::time_point now = Clock::now(); /* ns */
+        Clock::duration timeDiff = now - lastUpdate;
+        float timeDiffSecs = (float)timeDiff.count() / (float)1000000000; // Time diff in seconds
+
         io->prev.read_bytes  = io->curr.read_bytes;
         io->prev.write_bytes = io->curr.write_bytes;
 
@@ -18,7 +25,13 @@ void getIoStats(void *args) {
                 try_stoull(io->curr.write_bytes, line.substr(13));
             }
         }
+
         io->diff.read  = (io->curr.read_bytes  - io->prev.read_bytes) / (1024.f * 1024.f);
         io->diff.write = (io->curr.write_bytes - io->prev.write_bytes) / (1024.f * 1024.f);
+
+        io->per_second.read = io->diff.read / timeDiffSecs;
+        io->per_second.write = io->diff.write / timeDiffSecs;
+
+        lastUpdate = now;
     }
 }
