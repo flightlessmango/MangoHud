52ae70f6509a28c389a9685b0f173f64293b231a
FlightlessMango <flightlessmangoyt@gmail.com>
2021-12-13 14:33:39 +0100

    read_line: return empty line if ifstream fails

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -14,6 +14,9 @@ std::string read_line(const std::string& filename)
 {
     std::string line;
     std::ifstream file(filename);
+    if (file.fail()){
+        return line;
+    }
     std::getline(file, line);
     return line;
 }
