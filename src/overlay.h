ada173e67ff30412467217b0fff6f6ffc3b1baca
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2021-12-19 00:20:54 +0900

    overlay: avoid quadratic behavior
    Using a vector with erase(begin()) results in a copy of the full array
    every time. This is inefficient and in the case of HUD graphs, was having
    quadratic complexity which can noticeably affect performance.
    
    This patch replaces most of the operations with an O(1) alternative,
    keeping the total cost under a linear bound. While on this, also refactor
    the size of graph array into a constant.

--- src/overlay.h
+++ src/overlay.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdint.h>
 #include <vector>
+#include <deque>
 #include <imgui.h>
 #include "overlay_params.h"
 #include "iostats.h"
@@ -23,6 +24,8 @@ extern float g_overflow;
 struct frame_stat {
    uint64_t stats[OVERLAY_PLOTS_MAX];
 };
+
+static const int kMaxGraphEntries = 50;
 
 enum EngineTypes
 {
@@ -139,7 +142,7 @@ extern int32_t deviceID;
 extern struct benchmark_stats benchmark;
 extern ImVec2 real_font_size;
 extern std::string wineVersion;
-extern std::vector<logData> graph_data;
+extern std::deque<logData> graph_data;
 extern overlay_params *_params;
 
 void position_layer(struct swapchain_stats& data, struct overlay_params& params, ImVec2 window_size);
