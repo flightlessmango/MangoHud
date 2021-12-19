aab8730f69ba16b95d542650ab489e7b05d2d88d
FlightlessMango <flightlessmangoyt@gmail.com>
2021-12-19 14:42:11 +0100

    exec: make sure LD_PRELOAD is unset

--- src/logging.cpp
+++ src/logging.cpp
@@ -16,6 +16,8 @@ logData currentLogData = {};
 std::unique_ptr<Logger> logger;
 
 string exec(string command) {
+    if (getenv("LD_PRELOAD"))
+        unsetenv("LD_PRELOAD");
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
