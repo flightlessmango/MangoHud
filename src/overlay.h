41ce693e7a27b532850785c07e65701de156efc2
jackun <jack.un@gmail.com>
2022-03-06 11:15:19 +0200

    [vulkan] Per device font image(s)
    Less unnecessary recreations of font images.

--- src/overlay.h
+++ src/overlay.h
@@ -33,7 +33,6 @@ struct swapchain_stats {
 
    ImFont* font1 = nullptr;
    ImFont* font_text = nullptr;
-   size_t font_params_hash = 0;
    std::string time;
    double fps;
    uint64_t last_present_time;
@@ -107,7 +106,7 @@ void check_keybinds(overlay_params& params, uint32_t vendorID);
 void init_system_info(void);
 void FpsLimiter(struct fps_limit& stats);
 std::string get_device_name(uint32_t vendorID, uint32_t deviceID);
-void create_fonts(const overlay_params& params, ImFont*& small_font, ImFont*& text_font);
+void create_fonts(ImFontAtlas* font_atlas, const overlay_params& params, ImFont*& small_font, ImFont*& text_font);
 void right_aligned_text(ImVec4& col, float off_x, const char *fmt, ...);
 void center_text(const std::string& text);
 ImVec4 change_on_load_temp(LOAD_DATA& data, unsigned current);
