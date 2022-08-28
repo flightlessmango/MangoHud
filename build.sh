daeba885bf6ea9896bde703b8901ed182df43ec0
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2022-08-28 12:49:00 +0200

    build.sh: configure: make meson arguments default
    Instead of feeding -Dld_libdir_prefix=true
    etc... to meson, make them default
    in meson_options.txt
    
    Signed-off-by: Adel KARA SLIMANE <adel.ks@zegrapher.com>

--- build.sh
+++ build.sh
@@ -145,14 +145,14 @@ configure() {
     git submodule update --init --depth 50
     CONFIGURE_OPTS="-Dwerror=true"
     if [[ ! -f "build/meson64/build.ninja" ]]; then
-        meson build/meson64 --libdir lib/mangohud/lib64 --prefix /usr -Dappend_libdir_mangohud=false -Dld_libdir_prefix=true -Dld_libdir_abs=true $@ ${CONFIGURE_OPTS}
+        meson build/meson64 --libdir lib/mangohud/lib64 --prefix /usr $@ ${CONFIGURE_OPTS}
     fi
     if [[ ! -f "build/meson32/build.ninja" && "$MACHINE" = "x86_64" ]]; then
         export CC="gcc -m32"
         export CXX="g++ -m32"
         export PKG_CONFIG_PATH="/usr/lib32/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig:/usr/lib/pkgconfig:${PKG_CONFIG_PATH_32}"
         export LLVM_CONFIG="/usr/bin/llvm-config32"
-        meson build/meson32 --libdir lib/mangohud/lib32 --prefix /usr -Dappend_libdir_mangohud=false -Dld_libdir_prefix=true -Dld_libdir_abs=true $@ ${CONFIGURE_OPTS}
+        meson build/meson32 --libdir lib/mangohud/lib32 --prefix /usr $@ ${CONFIGURE_OPTS}
     fi
 }
 
