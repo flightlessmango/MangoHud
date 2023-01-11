c37c0de4fb4fc8243ceb6be68f2e166e751efbf3
FlightlessMango <flightlessmangoyt@gmail.com>
2023-01-11 07:47:25 +0100

    build.sh: add mangoapp to install/uninstall

--- build.sh
+++ build.sh
@@ -198,6 +198,7 @@ uninstall() {
     rm -fv "/usr/share/vulkan/implicit_layer.d/MangoHud.x86_64.json"
     rm -fv "/usr/bin/mangohud"
     rm -fv "/usr/bin/mangohud.x86"
+    rm -fv "/usr/bin/mangoapp"
 }
 
 install() {
@@ -237,6 +238,7 @@ install() {
     /usr/bin/install -Dvm644 ./build/release/usr/share/man/man1/mangohud.1 /usr/share/man/man1/mangohud.1
     /usr/bin/install -Dvm644 ./build/release/usr/share/doc/mangohud/MangoHud.conf.example /usr/share/doc/mangohud/MangoHud.conf.example
     /usr/bin/install -vm755  ./build/release/usr/bin/mangohud /usr/bin/mangohud
+    /usr/bin/install -vm755  ./build/release/usr/bin/mangoapp /usr/bin/mangoapp
 
     ln -sv $DEFAULTLIB /usr/lib/mangohud/lib
 
