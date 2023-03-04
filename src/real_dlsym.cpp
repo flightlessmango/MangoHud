240400a9f7aed7eed07a2f1ae44cb35e579dd1a5
Emil Velikov <emil.velikov@collabora.com>
2023-03-04 17:41:13 +0000

    meson: remove broken -Dwith_dlsym
    The option does not even build, so let's remove it.
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

--- src/real_dlsym.cpp
+++ src/real_dlsym.cpp
@@ -103,29 +103,3 @@ void* get_proc_address(const char* name) {
     void (*func)() = (void (*)())real_dlsym(RTLD_NEXT, name);
     return (void*)func;
 }
-
-#ifdef HOOK_DLSYM
-EXPORT_C_(void *) mangohud_find_glx_ptr(const char *name);
-EXPORT_C_(void *) mangohud_find_egl_ptr(const char *name);
-
-EXPORT_C_(void*) dlsym(void * handle, const char * name)
-{
-    void* func = nullptr;
-#ifdef HAVE_X11
-    func = mangohud_find_glx_ptr(name);
-    if (func) {
-        //fprintf(stderr,"%s: local: %s\n",  __func__ , name);
-        return func;
-    }
-#endif
-
-    func = mangohud_find_egl_ptr(name);
-    if (func) {
-        //fprintf(stderr,"%s: local: %s\n",  __func__ , name);
-        return func;
-    }
-
-    //fprintf(stderr,"%s: foreign: %s\n",  __func__ , name);
-    return real_dlsym(handle, name);
-}
-#endif
