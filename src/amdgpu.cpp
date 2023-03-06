7f8c05cef0ae90162ac85d586d083ae5a7389d58
bsolos <b.solomka@ukr.net>
2023-03-06 17:45:36 +0200

    Remove an unnecessary check in amdgpu_get_metrics

--- src/amdgpu.cpp
+++ src/amdgpu.cpp
@@ -173,10 +173,7 @@ void amdgpu_get_metrics(){
 
 	amdgpu_common_metrics_m.lock();
 
-	if (!do_libdrm_sampling) {
-		gpu_info.load = amdgpu_common_metrics.gpu_load_percent;
-	}
-
+	gpu_info.load = amdgpu_common_metrics.gpu_load_percent;
 	gpu_info.powerUsage = amdgpu_common_metrics.average_gfx_power_w;
 	gpu_info.CoreClock = amdgpu_common_metrics.current_gfxclk_mhz;
 	gpu_info.MemClock = amdgpu_common_metrics.current_uclk_mhz;
