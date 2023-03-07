e8f8bb479c2db2126cd03f13238f6b7543af0bd7
bsolos <b.solomka@ukr.net>
2023-03-07 13:17:14 +0200

    Move information used only by amdgpu_libdrm.cpp to the .cpp

--- src/amdgpu_libdrm.h
+++ src/amdgpu_libdrm.h
@@ -1,24 +1,5 @@
 #pragma once
 
-#define LIBDRM_MAX_DEVICES 32
-
-#define LIBDRM_SAMPLE_DELAY 3500
-#define LIBDRM_SAMPLE_BUF_SIZE 512
-
-#define LIBDRM_GRBM_STATUS 0x8010
-
-enum LIBDRM_GRBM_BITS {
-    LIBDRM_GRBM_BUSY_BIT = 1U << 31
-};
-
-struct libdrm_sample {
-    bool busy_bit;
-};
-
-struct libdrm_stats {
-    int busy;
-};
-
 extern std::string dri_device_path;
 extern bool do_libdrm_sampling;
 
