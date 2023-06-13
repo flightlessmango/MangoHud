801b5ef5fc88de8d3db0b34ad5d30651f4f4c5ea
FlightlessMango <flightlessmangoyt@gmail.com>
2023-06-13 18:53:57 +0200

    mangoapp: install by default
    [skip ci]

--- build.sh
+++ build.sh
@@ -197,6 +197,7 @@ uninstall() {
     rm -fv "/usr/bin/mangohud"
     rm -fv "/usr/bin/mangoplot"
     rm -fv "/usr/bin/mangohud.x86"
+    rm -fv "/usr/bin/mangoapp"
 }
 
 install() {
@@ -237,6 +238,7 @@ install() {
     /usr/bin/install -Dvm644 ./build/release/usr/share/doc/mangohud/MangoHud.conf.example /usr/share/doc/mangohud/MangoHud.conf.example
     /usr/bin/install -vm755  ./build/release/usr/bin/mangohud /usr/bin/mangohud
     /usr/bin/install -vm755  ./build/release/usr/bin/mangoplot /usr/bin/mangoplot
+    /usr/bin/install -vm755  ./build/release/usr/bin/mangoapp /usr/bin/mangoapp
 
     ln -sv $DEFAULTLIB /usr/lib/mangohud/lib
 
