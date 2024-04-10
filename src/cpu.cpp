ffff9b83b2b6b3c3e1e7eaae5b3a337446ae3443
flightlessmango <42713625+flightlessmango@users.noreply.github.com>
2024-04-11 00:26:48 +0200

    Revert "fix exception with std::stoi"
    This reverts commit 054f4cc9e3c12acc5bfb7f069af4b154e2c8e30b.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -469,8 +469,7 @@ static bool find_input(const std::string& path, const char* input_prefix, std::s
         if (uscore != std::string::npos) {
             file.erase(uscore, std::string::npos);
             input = path + "/" + file + "_input";
-            //9 characters should not overflow the 32-bit int
-            return std::stoi(read_line(input).substr(0, 9)) > 0;
+            return std::stoi(read_line(input)) > 0;
         }
     }
     return false;
