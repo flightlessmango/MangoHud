35ce9158fadff5a6c944b88f36230ca04f3af506
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-04-10 17:20:15 -0400

    HACK: check EGL load directory and only hook the first loaded libEGL

--- src/hook_dlsym.cpp
+++ src/hook_dlsym.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <link.h>
 #include "real_dlsym.h"
+#include <string.h>
+#include <unistd.h>
+
+#include <string>
+
+void *egl_handle = NULL;
 
 EXPORT_C_(void*) dlsym(void * handle, const char * name)
 {
@@ -14,6 +21,23 @@ EXPORT_C_(void*) dlsym(void * handle, const char * name)
     if (!find_egl_ptr)
         find_egl_ptr = reinterpret_cast<decltype(find_egl_ptr)> (real_dlsym(RTLD_NEXT, "mangohud_find_egl_ptr"));
 
+    char path_real[4096] = {0};
+    //pick a libEGL to use since ANGLE uses the same file name
+    if(handle != RTLD_DEFAULT && handle != RTLD_NEXT)
+    {
+        struct link_map *path;
+        dlinfo(handle, RTLD_DI_LINKMAP, &path);
+
+        int ret = readlink(path->l_name, path_real, 4096);
+        if (ret < 0) {
+            strcpy(path_real, path->l_name);
+        }
+
+        if (strstr(path_real, "libEGL") && !egl_handle) {
+            egl_handle = handle;
+        }
+    }
+
     void* func = nullptr;
     void* real_func = real_dlsym(handle, name);
 
@@ -25,7 +49,8 @@ EXPORT_C_(void*) dlsym(void * handle, const char * name)
         }
     }
 
-    if (find_egl_ptr && real_func) {
+    if (find_egl_ptr && real_func && egl_handle == handle) {
+        fprintf(stderr, "name %s\n", name);
         func = find_egl_ptr(name);
         if (func) {
             //fprintf(stderr,"%s: local: %s\n",  __func__ , name);
