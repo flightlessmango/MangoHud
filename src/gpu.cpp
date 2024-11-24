cf6c537e6681a5e97bd6a280468f89a362a66aad
flightlessmango <flightlessmangoyt@gmail.com>
2024-11-24 19:55:05 +0100

    ubuntu workflow: fixed active_gpu_found not used

--- src/gpu.cpp
+++ src/gpu.cpp
@@ -165,8 +165,8 @@ void GPUS::find_active_gpu() {
 
     // NVIDIA GPUs will not show up in fdinfo so we use NVML instead to find the active GPU
     // This will not work for older NVIDIA GPUs
-#ifdef HAVE_NVML
     if (!active_gpu_found) {
+#ifdef HAVE_NVML
         for (const auto& gpu : available_gpus) {
             // NVIDIA vendor ID is 0x10de
             if (gpu->vendor_id == 0x10de && gpu->nvidia->nvml_available) { 
@@ -180,8 +180,8 @@ void GPUS::find_active_gpu() {
 
             }
         }
-    }
 #endif
+    }
     SPDLOG_DEBUG("failed to find active GPU");
 }
 
