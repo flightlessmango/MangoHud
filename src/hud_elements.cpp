5687c44782366e3f20527f781d609240a5485f4f
17314642 <48242771@protonmail.ch>
2024-12-09 23:19:40 +0300

    hud_elements: now intel cards can display fan speed too
    stop being so mean to intel, they can now display fan speeds too since
    6.12 kernel.

--- src/hud_elements.cpp
+++ src/hud_elements.cpp
@@ -250,22 +250,20 @@ void HudElements::gpu_stats(){
                 ImGui::PopFont();
             }
 
-            if (HUDElements.vendorID == 0x1002 || HUDElements.vendorID == 0x10de){
-                if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_gpu_fan] && cpuStats.cpu_type != "APU"){
-                    ImguiNextColumnOrNewRow();
-                    right_aligned_text(text_color, HUDElements.ralign_width, "%i", gpu->metrics.fan_speed);
+            if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_gpu_fan] && !gpu->is_apu()){
+                ImguiNextColumnOrNewRow();
+                right_aligned_text(text_color, HUDElements.ralign_width, "%i", gpu->metrics.fan_speed);
+                ImGui::SameLine(0, 1.0f);
+                if (gpu->metrics.fan_rpm) {
+                    ImGui::PushFont(HUDElements.sw_stats->font1);
+                    HUDElements.TextColored(HUDElements.colors.text, "RPM");
+                } else {
+                    HUDElements.TextColored(HUDElements.colors.text, "%%");
+                    ImGui::PushFont(HUDElements.sw_stats->font1);
                     ImGui::SameLine(0, 1.0f);
-                    if (gpu->metrics.fan_rpm) {
-                        ImGui::PushFont(HUDElements.sw_stats->font1);
-                        HUDElements.TextColored(HUDElements.colors.text, "RPM");
-                    } else {
-                        HUDElements.TextColored(HUDElements.colors.text, "%%");
-                        ImGui::PushFont(HUDElements.sw_stats->font1);
-                        ImGui::SameLine(0, 1.0f);
-                        HUDElements.TextColored(HUDElements.colors.text, "FAN");
-                    }
-                    ImGui::PopFont();
+                    HUDElements.TextColored(HUDElements.colors.text, "FAN");
                 }
+                ImGui::PopFont();
             }
 
             if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_gpu_core_clock]){
