95908eac31b0178556356cc2720df5281bbe54aa
17314642 <48242771@protonmail.ch>
2025-03-26 22:45:10 +0300

    cpu: add match for cpu_thermal, instead of only cpu[0-9]_thermal

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -554,7 +554,7 @@ bool CPUStats::GetCpuFile() {
             // E2K (Elbrus 2000) CPU temperature module
             find_input(path, "temp", input, "Node 0 Max");
             break;
-        } else if (std::regex_match(name, match, std::regex("cpu\\d+_thermal"))) {
+        } else if (std::regex_match(name, match, std::regex("cpu\\d*_thermal"))) {
             find_fallback_input(path, "temp1", input);
             break;
         } else {
