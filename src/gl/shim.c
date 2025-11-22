160b9f75d766c2be7767d3bdfe6e131e285c25a1
andrew-2e128 <andrew.2e128@outlook.com>
2025-11-22 02:01:37 +0200

    shim: improve dlsym overriding behavior Overriding dlsym changes the symbols search scope, this makes real_dlsym fail to find symbols. This fix uses caller's handel to search for symbols if there is no handle defined after the first real_dlsym is called. This fix is needed for Nvidia OpenGL EGL on wayland.

--- src/gl/shim.c
+++ src/gl/shim.c
@@ -257,13 +257,43 @@ void* dlsym(void *handle, const char *name)
 {
     save_and_consume_real_dlerror();
     const char* dlsym_enabled = getenv("MANGOHUD_DLSYM");
+    const char* dlsym_RTLD_NEXT_env = getenv("MANGOHUD_DLSYM_RTLD_NEXT");
+    const bool bypass_RTLD_NEXT = dlsym_RTLD_NEXT_env ? dlsym_RTLD_NEXT_env[0] == '0' : false;
     void* is_angle = real_dlsym(handle, "eglStreamPostD3DTextureANGLE");
     // Consume error message if there was an error
     if (!is_angle)
     {
         (void) real_dlerror();
     }
+
     void* fn_ptr = real_dlsym(handle, name);
+    // Overriding dlsym makes it behave differently due to the change in search scope
+    // this results in name resolution failure when no handle or default handles are used
+    // as a mitigation we get a handle to the caller, and use it instead.
+    // This makes RTLD_DEFAULT behave better, but RTLD_NEXT is still broken.
+    // Note, RTLD_DEFAULT might still be broken for edge cases.
+    if (!fn_ptr && (handle == RTLD_DEFAULT || (handle == RTLD_NEXT && !bypass_RTLD_NEXT)))
+    {
+        void* ret_addr = __builtin_extract_return_addr(__builtin_return_address(0));
+        Dl_info dl_info = {};
+
+        if (dladdr(ret_addr, &dl_info) && dl_info.dli_fname && dl_info.dli_fname[0])
+        {
+            void* caller_handle = dlopen(dl_info.dli_fname, RTLD_LAZY | RTLD_NOLOAD);
+            if (caller_handle)
+            {
+                // last real_dlsym call failed so we consume the last error
+                (void) real_dlerror();
+                fn_ptr = real_dlsym(caller_handle, name);
+                if (handle == RTLD_NEXT)
+                {
+                    fprintf(stderr, "MANGOHUD: WARNING dlsym called with RTLD_NEXT, if you are facing issues try to disable mangohud.\n");
+                    fprintf(stderr, "MANGOHUD: or you can try MANGOHUD_DLSYM_RTLD_NEXT=0\n");
+                }
+            }
+        }
+    }
+
     // Activate override if there wasn't any new error
     // from real_dlsym
     activate_override = fn_ptr != NULL;
