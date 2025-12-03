d24ddd1302b4d8914f849ff3f5f1aa59ce72be51
17314642 <48242771@protonmail.ch>
2025-12-04 00:19:02 +0300

    params: add cpu_custom_temp_sensor
    This parameter accepts two arguments separated by comma:
      1) hwmon sensor name
      2) hwmon sensor file
    
    example:
       cpu_custom_temp_sensor=coretemp,temp7_input
    
       Will try to use hwmon sensor whose '/sys/class/hwmon/hwmon[0-9]+/name'
       file contents equals 'coretemp' and, and try to use 'temp7_input' file

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -572,11 +572,18 @@ bool CPUStats::GetCpuFile() {
         name = read_line(path + "/name");
         SPDLOG_DEBUG("hwmon: sensor name: {}", name);
 
-        if (name == "coretemp") {
+        std::map<std::string, std::string> custom_sensor = get_params()->cpu_custom_temp_sensor;
+
+        if (!custom_sensor["hwmon_name"].empty() && !custom_sensor["hwmon_input"].empty()) {
+            if (name != custom_sensor["hwmon_name"])
+                continue;
+
+            find_fallback_input(path, custom_sensor["hwmon_input"].c_str(), input);
+            break;
+        } else if (name == "coretemp") {
             find_input(path, "temp", input, "Package id 0");
             break;
-        }
-        else if ((name == "zenpower" || name == "k10temp")) {
+        } else if ((name == "zenpower" || name == "k10temp")) {
             if (!find_input(path, "temp", input, "Tdie"))
                 find_input(path, "temp", input, "Tctl");
             break;
