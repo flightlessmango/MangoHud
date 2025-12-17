d8686742f015714921599ec81cc2a706730899d8
flightlessmango <flightlessmangoyt@gmail.com>
2025-12-17 03:23:00 +0100

    imgui: update to 1.91.6

--- src/font.cpp
+++ src/font.cpp
@@ -39,8 +39,8 @@ void create_fonts(ImFontAtlas* font_atlas, const overlay_params& params, ImFont*
     config.MergeMode = true;
     // ImGui changed OversampleH default to 2, but it appears to sometimes cause
     // crashing issues in 32bit applications.
-    config.OversampleH = 0;
-    config.OversampleV = 0;
+    config.OversampleH = 1;
+    config.OversampleV = 1;
     config.PixelSnapH = true;
     static const ImWchar icon_ranges[] = { ICON_MIN_FK, ICON_MAX_FK, 0 };
 
