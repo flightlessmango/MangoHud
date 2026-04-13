35d859f6c3f55b20d015ab8ff9e28925679c9566
17314642 <48242771@protonmail.ch>
2026-04-14 00:50:28 +0300

    nvidia: add voltage via nvapi

---
+++ server/metrics/gpu/nvidia/nvapi_loader.cpp
@@ -0,0 +1,78 @@
+#include "nvapi_loader.hpp"
+#include <dlfcn.h>
+#include <spdlog/spdlog.h>
+
+static std::shared_ptr<libnvapi_loader> libnvapi_;
+
+std::shared_ptr<libnvapi_loader> get_libnvapi_loader()
+{
+    if (!libnvapi_)
+        libnvapi_ = std::make_shared<libnvapi_loader>();
+
+    return libnvapi_;
+}
+
+libnvapi_loader::libnvapi_loader() {
+    load();
+}
+
+libnvapi_loader::~libnvapi_loader() {
+    unload();
+}
+
+#define LOAD_NVAPI_FUNCTION(name, id) \
+    do {                    \
+        name = reinterpret_cast<decltype(this->name)>(nvapi_QueryInterface(id)); \
+        if (!name) { \
+            SPDLOG_ERROR("Failed to find address of {}", #name); \
+            unload(); \
+            return false; \
+        } \
+    } while(0)
+
+bool libnvapi_loader::load() {
+    if (loaded_)
+        return true;
+
+    library_ = dlopen(library_name.c_str(), RTLD_LAZY | RTLD_NODELETE);
+
+    if (!library_) {
+        SPDLOG_ERROR("Failed to open {}: {}", library_name, dlerror());
+        return false;
+    }
+
+    nvapi_QueryInterface =
+        reinterpret_cast<decltype(nvapi_QueryInterface)>(dlsym(library_, "nvapi_QueryInterface"));
+
+    if (!nvapi_QueryInterface) {
+        SPDLOG_ERROR("Failed to find address of nvapi_QueryInterface");
+        return false;
+    }
+
+    LOAD_NVAPI_FUNCTION(nvapi_Initialize, NVAPI_INIT_ID);
+    LOAD_NVAPI_FUNCTION(nvapi_GetErrorMessage, NVAPI_GET_ERROR_MESSAGE);
+    LOAD_NVAPI_FUNCTION(nvapi_EnumPhysicalGPUs, NVAPI_ENUM_PHYSICAL_GPUS);
+    LOAD_NVAPI_FUNCTION(nvapi_GPU_GetBusId, NVAPI_GET_BUS_ID);
+    LOAD_NVAPI_FUNCTION(nvapi_GetVoltage, NVAPI_GET_CURRENT_VOLTAGE);
+
+    loaded_ = true;
+    return true;
+}
+
+#undef LOAD_NVAPI_FUNCTION
+
+void libnvapi_loader::unload() {
+    if (library_) {
+        dlclose(library_);
+        library_ = NULL;
+    }
+
+    loaded_ = false;
+
+    nvapi_QueryInterface = nullptr;
+    nvapi_Initialize = nullptr;
+    nvapi_GetErrorMessage = nullptr;
+    nvapi_EnumPhysicalGPUs = nullptr;
+    nvapi_GPU_GetBusId = nullptr;
+    nvapi_GetVoltage = nullptr;
+}
---
+++ server/metrics/gpu/nvidia/nvapi_loader.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <atomic>
+
+#define NVAPI_MAX_PHYSICAL_GPUS 64
+#define NVAPI_INIT_ID 0x0150e828
+#define NVAPI_GET_ERROR_MESSAGE 0x6c2d048c
+#define NVAPI_ENUM_PHYSICAL_GPUS 0xe5ac921f
+#define NVAPI_GET_BUS_ID 0x1be0b8e5
+#define NVAPI_GET_CURRENT_VOLTAGE 0x465f9bcf
+
+class libnvapi_loader {
+public:
+    libnvapi_loader();
+    ~libnvapi_loader();
+
+    struct NvApiVoltage {
+        unsigned int version = sizeof(NvApiVoltage) | (1 << 16);
+        unsigned int flags;
+        unsigned int padding_1[8];
+        unsigned int value_microvolts;
+        unsigned int padding_2[8];
+    };
+
+    bool load();
+    bool is_loaded() { return loaded_; }
+
+    void* (*nvapi_QueryInterface)(unsigned int);
+    int (*nvapi_Initialize)();
+    int (*nvapi_GetErrorMessage)(int, char(*)[64]);
+    int (*nvapi_EnumPhysicalGPUs)(void*, unsigned int*);
+    int (*nvapi_GPU_GetBusId)(long, unsigned int*);
+    int (*nvapi_GetVoltage)(long, NvApiVoltage*);
+
+private:
+    void unload();
+    void* library_ = nullptr;
+
+    const std::string library_name = "libnvidia-api.so.1";
+    std::atomic<bool> loaded_ = false;
+
+    // Disallow copy constructor and assignment operator.
+    libnvapi_loader(const libnvapi_loader&);
+    void operator=(const libnvapi_loader&);
+};
+
+std::shared_ptr<libnvapi_loader> get_libnvapi_loader();
